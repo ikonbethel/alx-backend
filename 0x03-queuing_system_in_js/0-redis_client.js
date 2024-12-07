6cc8be2d357df89a9aaafffb939976a806ac9c99
Bethel Ikonne <46830724+ikonbethel@users.noreply.github.com>
2024-12-07 05:33:04 +0100

    Create 0-redis_client.js

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,14 @@
+// task 1: Node Redis Client
+import { createClient } from 'redis';
+
+const client = createClient({
+  url: 'redis://localhost:6379'
+});
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.log('Redis client not connected to the server:', err.toString());
+});
